176fb8c74d3c33e9bf5ba2a2e3d67a1dba1ffe5d
Lohithnj <lohithns03@gmail.com>
2025-12-19 13:30:29 +0530

    feat: Implement initial Quality Management Dashboard with inspection lot list and navigation.

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,10 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("com.kaar.quality.controller.Dashboard", {
         onInit: function () {
+            var oViewModel = new JSONModel({
+                totalLots: 0,
+                pendingResults: 0,
+                pendingUsage: 0
+            });
+            this.getView().setModel(oViewModel, "dashboard");
+
+            this._fetchCounts();
+        },
+
+        _fetchCounts: function () {
+            var oModel = this.getOwnerComponent().getModel("inspection");
+            var oViewModel = this.getView().getModel("dashboard");
+
+            // Fetch Total Lots
+            oModel.read("/ZBL_QUA_INSPECTION_VIEW/$count", {
+                success: function (sCount) {
+                    oViewModel.setProperty("/totalLots", sCount);
+                }
+            });
+
+            // Fetch Pending Lots (Assuming 'PENDING' is the code for both results and usage)
+            var aFilters = [new Filter("UsageDecisionCode", FilterOperator.EQ, "PENDING")];
+            oModel.read("/ZBL_QUA_INSPECTION_VIEW/$count", {
+                filters: aFilters,
+                success: function (sCount) {
+                    oViewModel.setProperty("/pendingResults", sCount);
+                    oViewModel.setProperty("/pendingUsage", sCount);
+                }
+            });
         },
 
         onPressAllLots: function () {
